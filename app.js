e5ef11722341b035f708ddeb31338c48f7444a37
Sohail Ahmad <sohailss2412@gmail.com>
2023-03-17 22:43:58 +0530

    removed node_modules folder

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 const express = require("express")
+const bodyParser = require("body-parser")
+
+const adminRouter = require("./routes/admin")
+const shopRouter = require("./routes/shop")
 
 const app = express()
 
-app.use("/add", (req, res, next) => {
-    console.log("In another middleware");
-    res.send("<h1>Hello from routed express</h1>")
-})
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(adminRouter)
+app.use(shopRouter)
 
-app.use("/", (req, res, next) => {
-    console.log("In another middleware");
-    res.send("<h1>Hello from express</h1>")
-})
-
-app.listen(3000)
-// const server = http.createServer(app)
-// server.listen(3000)
+app.listen(3000) 
